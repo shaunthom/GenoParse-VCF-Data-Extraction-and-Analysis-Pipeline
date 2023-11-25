133d0c9c4e23ac2c13c104ff033bd0df2fa1f915
Shaun Thomas <134566032+shaunthom@users.noreply.github.com>
2023-11-25 14:58:24 -0500

    Update and rename Extracting Info Field.py to Step 5 - Extracting Info Field.py

--- Extracting Info Field.py
+++
@@ -1,5 +0,0 @@
-info_field = []
-    numbers = [0,1]
-    for i in range(len(data)):
-        info_field.append(data[i]['INFO'])
-    return info_field
---
+++ Step 5 - Extracting Info Field.py
@@ -0,0 +1,13 @@
+info_field = []
+    numbers = [0,1]
+    for i in range(len(data)):
+        info_field.append(data[i]['INFO'])
+    return info_field
+
+
+"""
+
+a function that extracts the info field from the data dictionary that was 
+    created in the previous part. The function should return all the info field dictionaries as list.
+    
+"""
